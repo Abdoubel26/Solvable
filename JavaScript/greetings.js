4e8fc8e2ab803b89a206183f9be88cfbbf966274
Abdou Belounis <xenom442025@outlook.com>
2025-08-15 16:11:02 +0100

    Added an About and How It Works page with improved navigation buttons. and navigation bar

--- JavaScript/greetings.js
+++ JavaScript/greetings.js
@@ -5,9 +5,13 @@ const navbar = document.getElementById('navBar');
 function closeSiddeBar(){
     navbar.classList.remove("show");
     document.getElementById('close-sidebar-button').setAttribute('aria-expanded', 'true');
+     document.getElementById('open-sidebar-button').classList.remove('hidden');
 }
 
 function openSideBar(){
     navbar.classList.add("show");
     document.getElementById('close-sidebar-button').setAttribute('aria-expanded', 'true');
+    document.getElementById('open-sidebar-button').classList.add('hidden');
 }
+
+document.querySelector('.greeting').addEventListener('click', closeSiddeBar);
